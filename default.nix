9089657c514f8c6cbf7750fef03f152706459f7b
Ruud van Asseldonk <dev@veniogames.com>
2021-12-29 20:07:27 +0100

    Switch to Python instead
    I want to hack things quickly, and I want to experiment with the
    *application*, and not spend three days getting my http server and
    database connections set up, and I think I can do that faster in Python
    than I can in Rust. Let's switch to Python instead for now. Perhaps
    there will be a rewrite later, for now I just want to get the prototype
    working.

---
+++ default.nix
@@ -0,0 +1,26 @@
+# This file defines a Nix environment in which all required build tools and
+# dependencies are available. You do not *need* to use it, you can install
+# dependencies in any way you see fit. The advantage of the Nix environment is
+# that the Nixpkgs revision is pinned, and thereby the versions of all tools. If
+# you can build a commit today, you should be able to build it three years from
+# now. The same may not be true if you use the distro-provided versions. You can
+# start a shell with dependencies available by running `nix run` in the root of
+# the repository.
+
+let
+  pkgs = (import ./nixpkgs-pinned.nix) {};
+  python = pkgs.python3.withPackages (ps: [
+    ps.flask
+    ps.jinja2
+    ps.psycopg2
+  ]);
+in
+  pkgs.buildEnv {
+    name = "hanson-devenv";
+    paths = [
+      pkgs.black
+      pkgs.mypy
+      pkgs.postgresql
+      python
+    ];
+  }
---
+++ nixpkgs-pinned.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://github.com/nixos/nixpkgs/archive/ac169ec6371f0d835542db654a65e0f2feb07838.tar.gz";
+  sha256 = "0bwjyz15sr5f7z0niwls9127hikp2b6fggisysk0cnk3l6fa8abh";
+})

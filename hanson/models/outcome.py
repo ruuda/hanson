6f82c821153234562138380db47de0632e1b38f6
Ruud van Asseldonk <dev@veniogames.com>
2022-03-29 22:57:38 +0200

    Add schema for outcomes
    And I am going to over-engineer this from the start and add support for
    not only discrete outcomes, but also continuous outcomes, and both in
    datetime format as well as arbitrary floats (e.g. for estimating
    monetary values).

---
+++ hanson/models/outcome.py
@@ -0,0 +1,165 @@
+from __future__ import annotations
+
+from typing import Iterable, List, NamedTuple, Optional, Tuple, Union
+from datetime import datetime
+from dataclasses import dataclass
+
+from hanson.database import Transaction
+
+
+@dataclass(frozen=True)
+class Outcome:
+    id: int
+    market_id: int
+    name: str
+    color: str
+
+    @staticmethod
+    def create_discrete(
+        tx: Transaction,
+        market_id: int,
+        name: str,
+        color: str,
+    ) -> OutcomeDiscrete:
+        with tx.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO "outcome" (market_id, name, color)
+                VALUES (%s, %s, %s)
+                RETURNING id;
+                """,
+                (market_id, name, color),
+            )
+            outcome_id = cur.fetchone()[0]
+            return OutcomeDiscrete(outcome_id, market_id, name, color)
+
+    @staticmethod
+    def create_float(
+        tx: Transaction,
+        market_id: int,
+        name: str,
+        color: str,
+        value: float,
+    ) -> OutcomeFloat:
+        with tx.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO "outcome" (market_id, name, color, value_float)
+                VALUES (%s, %s, %s, %s)
+                RETURNING id;
+                """,
+                (market_id, name, color, value),
+            )
+            outcome_id = cur.fetchone()[0]
+            return OutcomeFloat(outcome_id, market_id, name, color, value)
+
+    @staticmethod
+    def create_datetime(
+        tx: Transaction,
+        market_id: int,
+        name: str,
+        color: str,
+        value: datetime,
+    ) -> OutcomeDatetime:
+        assert value.tzinfo is not None
+        with tx.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO "outcome" (market_id, name, color, value_datetime)
+                VALUES (%s, %s, %s, %s)
+                RETURNING id;
+                """,
+                (market_id, name, color, value),
+            )
+            outcome_id = cur.fetchone()[0]
+            return OutcomeDatetime(outcome_id, market_id, name, color, value)
+
+    @staticmethod
+    def get_all_by_market_unchecked(
+        tx: Transaction, market_id: int
+    ) -> Iterable[Outcome]:
+        """
+        Iterate all outcomes for a given market, but don't check that the type
+        is consistent for all outcomes.
+        """
+        with tx.cursor() as cur:
+            cur.execute(
+                """
+                SELECT
+                  id,
+                  name,
+                  color,
+                  value_float,
+                  value_datetime
+                FROM
+                  "outcome"
+                WHERE
+                  market_id = %s
+                """,
+                (market_id,),
+            )
+            while True:
+                row: Optional[
+                    Tuple[int, str, str, Optional[float], Optional[datetime]]
+                ] = cur.fetchone()
+                if row is not None:
+                    id, name, color, value_float, value_datetime = row
+                    if value_float is not None:
+                        yield OutcomeFloat(id, market_id, name, color, value_float)
+                    elif value_datetime is not None:
+                        yield OutcomeDatetime(
+                            id, market_id, name, color, value_datetime
+                        )
+                    else:
+                        yield OutcomeDiscrete(id, market_id, name, color)
+
+    @staticmethod
+    def get_all_by_market(tx: Transaction, market_id: int) -> Outcomes:
+        outcomes = list(Outcome.get_all_by_market_unchecked(tx, market_id))
+        assert len(outcomes) >= 2, "A market must have at least two outcomes."
+
+        # Verify that all elements are of the same type, and then put things in
+        # a more strongly typed container that indicates that. Unfortunately
+        # Mypy cannot check this for us.
+
+        if isinstance(outcomes[0], OutcomeFloat):
+            assert all(isinstance(oc, OutcomeFloat) for oc in outcomes)
+            return OutcomesFloat(outcomes)  # type: ignore[arg-type]
+
+        elif isinstance(outcomes[0], OutcomeDatetime):
+            assert all(isinstance(oc, OutcomeDatetime) for oc in outcomes)
+            return OutcomesDatetime(outcomes)  # type: ignore[arg-type]
+
+        else:
+            assert all(isinstance(oc, OutcomeDiscrete) for oc in outcomes)
+            return OutcomesDiscrete(outcomes)  # type: ignore[arg-type]
+
+
+@dataclass(frozen=True)
+class OutcomeDiscrete(Outcome):
+    value: None = None
+
+
+@dataclass(frozen=True)
+class OutcomeFloat(Outcome):
+    value: float
+
+
+@dataclass(frozen=True)
+class OutcomeDatetime(Outcome):
+    value: datetime
+
+
+class OutcomesDiscrete(NamedTuple):
+    outcomes: List[OutcomeDiscrete]
+
+
+class OutcomesFloat(NamedTuple):
+    outcomes: List[OutcomeFloat]
+
+
+class OutcomesDatetime(NamedTuple):
+    outcomes: List[OutcomeDatetime]
+
+
+Outcomes = Union[OutcomesDiscrete, OutcomesFloat, OutcomesDatetime]
--- hanson/util/dummy_data.py
+++ hanson/util/dummy_data.py
@@ -4,7 +4,9 @@ from hanson.database import Transaction, connect_default
 from hanson.models.user import User
 from hanson.models.session import Session
 from hanson.models.market import Market
+from hanson.models.outcome import Outcome
 
+from datetime import datetime, timezone
 from typing import List
 from textwrap import dedent
 
@@ -20,8 +22,9 @@ def add_sessions(tx: Transaction, users: List[User]) -> List[Session]:
 
 
 def add_markets(tx: Transaction, users: List[User]) -> List[Market]:
-    return [
-        Market.create(
+    markets = []
+    for user in users:
+        market = Market.create(
             tx,
             author_user_id=user.id,
             title=f"Will {user.username.title()} frobnicate the widget this year?",
@@ -39,8 +42,71 @@ def add_markets(tx: Transaction, users: List[User]) -> List[Market]:
                 """,
             ),
         )
-        for user in users
-    ]
+        Outcome.create_discrete(tx, market.id, "Yes", "#68a223")
+        Outcome.create_discrete(tx, market.id, "No", "#a22a17")
+        markets.append(market)
+
+        market = Market.create(
+            tx,
+            author_user_id=user.id,
+            title=f"What will {user.username.title()} have for lunch?",
+            description=dedent(
+                f"""
+                {user.username.title()} is known to have bread with peanut
+                butter for lunch *every single day*. But perhaps today will be
+                different?
+                """,
+            ),
+        )
+        Outcome.create_discrete(tx, market.id, "Peanut butter", "#b58800")
+        Outcome.create_discrete(tx, market.id, "Cheese", "#d2bb00")
+        Outcome.create_discrete(tx, market.id, "Ham", "#d58b8b")
+        markets.append(market)
+
+        market = Market.create(
+            tx,
+            author_user_id=user.id,
+            title=f"When will {user.username.title()} graduate?",
+            description=dedent(
+                f"""
+                {user.username.title()} has been studying for four year already,
+                but still hasn’t passed the freshman course “Linear Algebra I”.
+                Will {user.username.title()} complete it this year?
+                """,
+            ),
+        )
+        # TODO: Should all outcomes have a name? Maybe float and datetime outcomes
+        # should be named by the system instead.
+        t = datetime(2022, 6, 1, 0, 0, 0, tzinfo=timezone.utc)
+        Outcome.create_datetime(tx, market.id, "2022-06", "#ff0000", t)
+        Outcome.create_datetime(
+            tx, market.id, "2023-06", "#880088", t.replace(year=2023)
+        )
+        Outcome.create_datetime(
+            tx, market.id, "2024-06", "#0000ff", t.replace(year=2024)
+        )
+        markets.append(market)
+
+        market = Market.create(
+            tx,
+            author_user_id=user.id,
+            title=f"How many liters of milk can {user.username.title()} drink?",
+            description=dedent(
+                f"""
+                We are going to hold a milk drinking contest. For two hours,
+                participants can drink as much milk as they can. How many liters
+                of milk will {user.username.title()} consume during this
+                contest?
+                """,
+            ),
+        )
+        Outcome.create_float(tx, market.id, "0.2", "#ff0000", 0.2)
+        Outcome.create_float(tx, market.id, "0.5", "#aa0022", 0.5)
+        Outcome.create_float(tx, market.id, "1.0", "#2200aa", 1.0)
+        Outcome.create_float(tx, market.id, "2.0", "#0000ff", 2.0)
+        markets.append(market)
+
+    return markets
 
 
 def main() -> None:

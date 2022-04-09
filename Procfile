71e815b2da08e482fd3ec935f64dc98ba5967883
Ruud van Asseldonk <dev@veniogames.com>
2022-04-09 23:56:57 +0200

    Connect to Postgres from Haskell app
    It needs more work, like a transaction abstraction like I have in
    Python, running it at serializable isolation level, etc. But for now, I
    can query the database, yay.
    
    Also, the binary is so fast to start now that on the first Overmind run,
    it tries to connect before Postgres is ready. I should make it retry. Or
    maybe it needs a connection pool and connect lazily or something.

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 # web: FLASK_ENV=development python -m flask run --port $PORT
 web: git ls-files | entr -r make run
-db:  PGHOST=$PWD/run/db_dev/socket tools/run_postgres.py run/db_dev
+db:  tools/run_postgres.py run/db_dev
--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
 import Control.Monad.IO.Class (liftIO)
-import Data.Default.Class as Default
-import Data.Text as Text
-import Web.Scotty as Scotty
-
+import Database.PostgreSQL.Simple (Only (..), connectPostgreSQL)
 import Text.Blaze (Markup)
+import Data.Text (Text)
 
+import qualified Data.Default.Class as Default
+import qualified Data.Text as Text
+import qualified Database.PostgreSQL.Simple as Postgres
 import qualified Text.Blaze as Blaze
 import qualified Text.Blaze.Html.Renderer.Utf8 as BlazeUtf8
 import qualified Text.Blaze.Html5 as Html
 import qualified Text.Blaze.Html5.Attributes as Attr
+import qualified Web.Scotty as Scotty
 
-renderMarket :: Int -> Markup
-renderMarket i = Html.article $ do
-  Html.h1 $ Blaze.text $ "Market " <> (Text.pack $ show i)
+renderMarket :: Text -> Markup
+renderMarket title = Html.article $ do
+  Html.h1 $ Blaze.text $ "Market " <> title
   Html.p $ Blaze.text "This is a market."
 
 serveHtml :: Markup -> Scotty.ActionM ()
@@ -26,10 +28,13 @@ serveHtml markup = do
   Scotty.raw $ BlazeUtf8.renderHtml markup
 
 main :: IO ()
-main = Scotty.scottyOpts Default.def $ do
+main = do
+  conn <- connectPostgreSQL ""
 
-  Scotty.get "/market/:market_id" $ do
-    marketId <- Scotty.param "market_id"
-    serveHtml $ renderMarket $ marketId
-    liftIO $ putStrLn $ "Hi from market."
+  Scotty.scottyOpts Default.def $ do
+    Scotty.get "/market/:market_id" $ do
+      marketId :: Int <- Scotty.param "market_id"
+      [Only (title :: Text)] <- liftIO $ Postgres.query conn "SELECT market_current_title(?);" (Only marketId)
+      serveHtml $ renderMarket $ title
+      liftIO $ putStrLn $ "Hi from market."
   

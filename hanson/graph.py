a8dfd6a45adaf24d0db812f32cc62f5acd8324dd
Ruud van Asseldonk <dev@veniogames.com>
2022-10-19 23:11:24 +0200

    Toy around with axis ticks
    This is hard to get right, either I need to integrate it with the
    binning (which would probably be a good idea), or I need to make the
    ticker decide based on the current and previous value, or both.

--- hanson/graph.py
+++ hanson/graph.py
@@ -5,23 +5,87 @@
 # you may not use this file except in compliance with the License.
 # A copy of the License has been included in the root of the repository.
 
-from datetime import datetime, timezone
-from typing import Iterable
+from __future__ import annotations
+
+from datetime import datetime, timedelta, timezone
+from typing import Iterable, NamedTuple, Optional
 
 from hanson.models.history import ProbabilityHistory
 from hanson.models.outcome import Outcome
 
 
-def tick_label(bin_size_secs: int, t: datetime) -> str:
-    assert t.tzinfo is not None
-    t = t.astimezone(timezone.utc)
+class TickFormat(NamedTuple):
+    timetuple_index: str
+    timetuple_multiple: int
+    format: str
 
-    if bin_size_secs <= 3600 * 3:
-        return t.strftime("%H:%M")
-    elif bin_size_secs < 3600 * 24 * 3:
-        return t.strftime("%b %d %H:%M")
-    else:
-        return t.strftime("%Y-%m-%d")
+    @staticmethod
+    def time_axis_ticks(start_time: datetime, end_time: datetime) -> TickFormat:
+        """
+        Return suitable ticks for a given time range.
+        """
+        assert start_time.tzinfo is not None
+        assert end_time.tzinfo is not None
+
+        y0, m0, d0, hh0, mm0, *_ = start_time.utctimetuple()
+        y1, m1, d1, hh1, mm1, *_ = end_time.utctimetuple()
+        duration = end_time - start_time
+
+        format_parts = ["", "", "%H:%M"]
+        index = "hours"
+        multiple = 1
+
+        if d1 > d0 or m1 > m0:
+            format_parts[1] = "%b %d"
+
+        if y1 > y0:
+            format_parts[0] = "%Y"
+
+        if duration > timedelta(hours=12):
+            multiple = 8
+
+        if duration > timedelta(days=7):
+            index = "days"
+            format_parts[2] = ""
+            multiple = 2
+
+        if duration > timedelta(days=14):
+            multiple = 5
+
+        if duration > timedelta(days=30):
+            multiple = 15
+
+        if duration > timedelta(days=60):
+            index = "months"
+            multiple = 1
+
+        return TickFormat(
+            index,
+            multiple,
+            " ".join(p for p in format_parts if p != ""),
+        )
+
+    def get_tick(self, t: datetime) -> Optional[str]:
+        """
+        Format a tick, if we should tick at this particular time.
+        """
+        assert t.tzinfo is not None
+
+        y, m, d, h, *_ = t.utctimetuple()
+
+        if self.timetuple_index == "hours":
+            if h % self.timetuple_multiple > 0:
+                return None
+
+        elif self.timetuple_index == "days":
+            if (d - 1) % self.timetuple_multiple > 0:
+                return None
+
+        elif self.timetuple_index == "months":
+            if d > 1:
+                return None
+
+        return t.strftime(self.format)
 
 
 def render_graph(
@@ -54,7 +118,8 @@ def render_graph(
     current_tick = end_tick
     current_time = end_time
     current_elem = len(ps_history.history) - 1
-    n_until_tick = 0
+
+    tick_format = TickFormat.time_axis_ticks(start_time, end_time)
 
     while current_tick > start_tick:
         current_time, current_ps = ps_history.history[current_elem]
@@ -80,22 +145,17 @@ def render_graph(
             )
             start_y += p * axis_height
 
-        if n_until_tick == 0:
-            label = tick_label(
-                bin_size_secs=bin_size_secs,
-                t=datetime.fromtimestamp(current_tick * bin_size_secs, tz=timezone.utc),
-            )
+        t = datetime.fromtimestamp(current_tick * bin_size_secs, tz=timezone.utc)
+        tick_label = tick_format.get_tick(t)
+        if tick_label is not None:
             result.append(
                 f'<circle cx="{x - 0.5:.2f}" '
                 f'cy="{axis_height + 0.4:.2f}" '
                 f'r="0.1"></circle>'
                 f'<text x="{x - 0.5:.2f}" '
                 f'y="{axis_height + 1.2:.2f}" '
-                f'text-anchor="middle">{label}</text>'
+                f'text-anchor="middle">{tick_label}</text>'
             )
-            n_until_tick = 8
-        else:
-            n_until_tick -= 1
 
         current_tick -= 1
 

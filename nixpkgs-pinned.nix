f2c4e5e88e2df99b489543f4277560ec9d68b80c
Ruud van Asseldonk <dev@veniogames.com>
2023-03-02 23:11:14 +0100

    Delete old Nix development environment
    I kept it around because for some strange reason, my tests passed in it,
    but there was a hang in the new flake-based devenv. But now that I
    updated to a newer Nixpkgs and Python version, the hang is no longer
    there, so we no longer need the old Nix environment.

--- nixpkgs-pinned.nix
+++
@@ -1,4 +0,0 @@
-import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/fcd48a5a0693f016a5c370460d0c2a8243b882dc.tar.gz";
-  sha256 = "06nyvac1azpa9gqdhj9py6ljsvbfgx49ilp8kf6w0w9clxba64vg";
-})
